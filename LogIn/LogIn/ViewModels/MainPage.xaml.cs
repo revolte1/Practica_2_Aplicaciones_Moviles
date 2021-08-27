377157b494392f4c1dd82794e1047340fc0c71b6
ernesto melendez <1067087@est.intec.edu.do>
2021-08-27 01:08:46 -0400

    Add project files.

---
+++ LogIn/LogIn/ViewModels/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace LogIn
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnButtonClicked(object sender, EventArgs e)
+        {
+            if (userEntry.Text == "Admin" && passwordEntry.Text == "123")
+            {
+                await DisplayAlert("Saludos!", "Bienvenido: Admin", "Ok");
+                await Navigation.PushAsync(new HomePage());
+            }
+            else
+            {
+                await DisplayAlert("Alerta", "No pueden existir campos vacíos", "Ok");
+            }
+
+        }
+        private async void OnButtonClicked2(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SignUp());
+        }
+
+
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+https://github.com/revolte1/Practica_1_LogIn/blob/master/Screenshots/Pagina%20de%20Inicio.png
+https://github.com/revolte1/Practica_1_LogIn/blob/master/Screenshots/Error%20Campos%20Vac%C3%ADos.png
+https://github.com/revolte1/Practica_1_LogIn/blob/master/Screenshots/LogIn%20Exitoso.png
